8fb69ade4358b72cbd5610cb792753ae7ecebea9
Allison Stewart <amstewart@yahoo.com>
2021-11-11 16:21:07 -0500

    completed task 2

--- index.js
+++ index.js
@@ -82,7 +82,26 @@ console.log(ben.stomach);
 */
 
 class Car {
-  
+  constructor(model, milesPerGallon){
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons){
+    this.tank += gallons;
+  }
+  drive(distance){
+    let totalDrivableDistance = this.tank * this.milesPerGallon
+    if (distance > totalDrivableDistance) {
+      this.odometer += totalDrivableDistance;
+      this.tank = 0
+      return `I ran out of fuel at ${this.odometer} miles!`
+    }else {
+      this.odometer += distance;
+      this.tank = this.tank - (distance / this.milesPerGallon)
+    }
+  }
 }
 
 /*
